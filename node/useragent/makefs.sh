19b2af87dd9123b9d5501c65bd9f18616891a7a8
Caspar Norée Palm <caspar.noreepalm@gmail.com>
2025-04-18 17:53:02 +0200

    build mke2fs

--- node/useragent/makefs.sh
+++ node/useragent/makefs.sh
@@ -7,7 +7,36 @@ cd "$(dirname "$0")"
 
 cargo build --target=x86_64-unknown-linux-musl --release
 
-echo Static init built! Generating rootfs
+# If busybox is not at target/busybox, download it
+if [ ! -f target/busybox ]; then
+    echo Downloading busybox
+    wget -O target/busybox https://busybox.net/downloads/binaries/1.35.0-x86_64-linux-musl/busybox
+fi
+
+# crun from https://github.com/containers/crun/releases/download/1.21/crun-1.21-linux-amd64
+# Download crun if not present
+
+if [ ! -f target/crun ]; then
+    echo Downloading crun
+    wget -O target/crun https://github.com/containers/crun/releases/download/1.21/crun-1.21-linux-amd64
+fi
+
+# If mke2fs is not at /sbin/mke2fs, build it
+if [ ! -f target/mke2fs ]; then
+    echo Building mke2fs
+    wget -O target/e2fsprogs-1.47.1.tar.gz https://mirrors.edge.kernel.org/pub/linux/kernel/people/tytso/e2fsprogs/v1.47.1/e2fsprogs-1.47.1.tar.gz
+    tar -xzf target/e2fsprogs-1.47.1.tar.gz -C target
+    cd target/e2fsprogs-1.47.1
+    ./configure CFLAGS='-g -static -O2 -no-pie' LDFLAGS="-static"
+    make -j$(nproc)
+    cd ../..
+    cp target/e2fsprogs-1.47.1/misc/mke2fs target/mke2fs
+    chmod +x target/mke2fs
+    rm target/e2fsprogs-1.47.1.tar.gz
+    rm -rf target/e2fsprogs-1.47.1
+fi
+
+echo Dependencies finished. Generating the rootfs
 
 rm -f target/rootfs.ext4 || true
 
@@ -29,11 +58,6 @@ echo Coping the static init to the rootfs
 sudo cp target/x86_64-unknown-linux-musl/release/nodeagent target/nodeagent_tmp_rootfs/sbin/init
 sudo chmod +x target/nodeagent_tmp_rootfs/sbin/init
 
-# If busybox is not at target/busybox, download it
-if [ ! -f target/busybox ]; then
-    echo Downloading busybox
-    wget -O target/busybox https://busybox.net/downloads/binaries/1.35.0-x86_64-linux-musl/busybox
-fi
 sudo cp target/busybox target/nodeagent_tmp_rootfs/sbin/busybox
 sudo chmod +x target/nodeagent_tmp_rootfs/sbin/busybox
 
@@ -41,17 +65,9 @@ sudo ln -s /sbin/busybox target/nodeagent_tmp_rootfs/bin/busybox
 
 sudo chroot target/nodeagent_tmp_rootfs /sbin/busybox --install -s /bin
 
-# Copy mkefs to the rootfs
-sudo cp /sbin/mke2fs target/nodeagent_tmp_rootfs/sbin/mke2fs
+sudo cp target/mke2fs target/nodeagent_tmp_rootfs/sbin/mke2fs
 sudo chmod +x target/nodeagent_tmp_rootfs/sbin/mke2fs
 
-# crun from https://github.com/containers/crun/releases/download/1.21/crun-1.21-linux-amd64
-# Download crun if not present
-
-if [ ! -f target/crun ]; then
-    echo Downloading crun
-    wget -O target/crun https://github.com/containers/crun/releases/download/1.21/crun-1.21-linux-amd64
-fi
 sudo cp target/crun target/nodeagent_tmp_rootfs/bin/crun
 sudo chmod +x target/nodeagent_tmp_rootfs/bin/crun
 
